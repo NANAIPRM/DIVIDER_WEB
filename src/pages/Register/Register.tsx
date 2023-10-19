91865ae0bfe59668bb2f5b567002159d1824834e
NANAIPRM <porramatc@baania.com>
2023-10-19 23:01:24 +0700

    update

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import FormRegister from './components/formRegister'
+import FormRegister from './components/FormRegister'
 
 function Register() {
     return (
-        <div>
+        <div className="bg-black flex justify-around h-screen ">
             <FormRegister />
+            <img
+                className="w-1/2"
+                src="https://cdn.gencraft.com/prod/user/9e52aaa7-2661-4081-89be-7aac696e3969/82f5c2ed-ccc0-4584-9a20-a3ec32329d9e/images/image1_0.jpg?Expires=1697816970&Signature=ji8HYESfP31xbBUDBgW7lO8DKZNJXHbccWxi1XiYgbdZKCbLh9DsVLqEdSYTS~w1dd37b5ql446kRouOvfkcUcA8bs76Vbap94FrXENEnk2-7tzttRu-jdTqIoanGRRY-pMoMQ6ZcYNws2kSox77nThrENaROHbEMtD288ULWJ5hpRYBjm-rp6rzZIzhF~9yCsMUgjJ9ia~I~ek71LDNh0cnurKk-OEsmQPz0oljV1YaoS2lGl0vEewORxUjkdPvUPV1LP03d9JACR8FjVwyZ5hKcgvg5s5SqRfmmolJVutQgy6NmYD9dmKgYk6NCqQY18K8KfmoXBw5PPqFmlOGxA__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+            />
         </div>
     )
 }
--- src/pages/Register/components/FormRegister.tsx
+++ src/pages/Register/components/FormRegister.tsx
@@ -18,24 +18,27 @@ interface Errors {
 }
 
 function FormRegister() {
-    const [formData, setFormData] = useState<FormData>({
+    const defaultFormData: FormData = {
         firstName: '',
         lastName: '',
         email: '',
         password: '',
         confirmPassword: '',
-    })
+    }
 
+    const [formData, setFormData] = useState<FormData>(defaultFormData)
     const [errors, setErrors] = useState<Errors>({})
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target
         setFormData({ ...formData, [name]: value })
+        // Clear the corresponding error message when the user makes changes
+        setErrors({ ...errors, [name]: '' })
     }
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault()
-        const validationErrors = validateRegister(formData) as Errors // Specify the type here
+        const validationErrors = validateRegister(formData) as Errors
         if (Object.keys(validationErrors).length === 0) {
             // The form is valid, you can submit it here
             console.log('Form is valid, submitting...')
@@ -46,63 +49,84 @@ function FormRegister() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
+        <form
+            onSubmit={handleSubmit}
+            className="flex flex-col items-center justify-center w-1/2 m-auto gap-5 bg-white"
+        >
+            <div className="flex flex-col gap-5 w-3/4">
                 <label htmlFor="firstName">First Name</label>
                 <input
                     type="text"
                     name="firstName"
                     value={formData.firstName}
                     onChange={handleChange}
+                    placeholder="Enter your first name"
                 />
-                {errors.firstName && <p>{errors.firstName}</p>}
+                {errors.firstName && (
+                    <p className="text-red-500">{errors.firstName}</p>
+                )}
             </div>
 
-            <div>
+            <div className="flex flex-col gap-5 w-3/4">
                 <label htmlFor="lastName">Last Name</label>
                 <input
                     type="text"
                     name="lastName"
                     value={formData.lastName}
                     onChange={handleChange}
+                    placeholder="Enter your last name"
                 />
-                {errors.lastName && <p>{errors.lastName}</p>}
+                {errors.lastName && (
+                    <p className="text-red-500">{errors.lastName}</p>
+                )}
             </div>
 
-            <div>
+            <div className="flex flex-col gap-5 w-3/4">
                 <label htmlFor="email">Email</label>
                 <input
-                    type="text"
+                    type="email"
                     name="email"
                     value={formData.email}
                     onChange={handleChange}
+                    placeholder="Enter your email"
                 />
-                {errors.email && <p>{errors.email}</p>}
+                {errors.email && <p className="text-red-500">{errors.email}</p>}
             </div>
 
-            <div>
+            <div className="flex flex-col gap-5 w-3/4">
                 <label htmlFor="password">Password</label>
                 <input
                     type="password"
                     name="password"
                     value={formData.password}
                     onChange={handleChange}
+                    placeholder="Enter your password"
                 />
-                {errors.password && <p>{errors.password}</p>}
+                {errors.password && (
+                    <p className="text-red-500">{errors.password}</p>
+                )}
             </div>
 
-            <div>
+            <div className="flex flex-col gap-5 w-3/4">
                 <label htmlFor="confirmPassword">Confirm Password</label>
                 <input
                     type="password"
                     name="confirmPassword"
                     value={formData.confirmPassword}
                     onChange={handleChange}
+                    placeholder="Confirm your password"
                 />
-                {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+                {errors.confirmPassword && (
+                    <p className="text-red-500">{errors.confirmPassword}</p>
+                )}
             </div>
 
-            <button type="submit">Register</button>
+            <button
+                type="submit"
+                className="bg-blue-500 text-white py-2 px-4 rounded-full"
+            >
+                Register
+            </button>
         </form>
     )
 }
